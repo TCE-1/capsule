7f516cd854f5c772cf2fd1bbc4ed765f8df7ea29
TCE-1 <tce.dev42@gmail.com>
2020-02-22 12:07:04 -0500

    picture access is done dynamicaly now

--- src/App.js
+++ src/App.js
@@ -4,57 +4,36 @@ import sty from './ImageDisplay.module.css';
 import ImageGallery1 from './imageGallery1';
 import ImageGallery2 from './imageGallery1';
 import ImageGallery3 from './imageGallery1';
+import Dropdown from './ddm/ddm';
 
+//--The const below determine the number of files in each folder and send the
+//--built/bundled path to components to be rendered.
+const pantsFiles= require.context('./images/pants', false, /.*\.jpeg$/ );
+const pantsArr= pantsFiles.keys() ;
 
-let urls = [
-  require("./images/1.jpg"),
-  require("./images/2.jpg"),
-  require("./images/3.jpg")
-]
+const shirtsFiles= require.context('./images/shirts', false, /.*\.jpeg$/ );
+const shirtsArr= shirtsFiles.keys() ;
 
-let pants= [
-  require("./images/pants/1.jpeg"),
-  require("./images/pants/2.jpeg"),
-  require("./images/pants/3.jpeg"),
-  require("./images/pants/4.jpeg"),
-  require("./images/pants/5.jpeg"),
-  require("./images/pants/6.jpeg"),
-  require("./images/pants/7.jpeg"),
-  require("./images/pants/8.jpeg")
-]
+const jacketsFiles = require.context('./images/jackets', false, /.*\.jpeg$/ );
+const jacketsArr= jacketsFiles.keys() ;
 
-let shirts= [
-  require("./images/shirts/1.jpeg"),
-  require("./images/shirts/2.jpeg"),
-  require("./images/shirts/3.jpeg"),
-  require("./images/shirts/4.jpeg"),
-  require("./images/shirts/5.jpeg"),
-  require("./images/shirts/6.jpeg"),
-  require("./images/shirts/7.jpeg"),
-  require("./images/shirts/8.jpeg")
-]
-
-let jackets= [
-  require("./images/jackets/1.jpeg"),
-  require("./images/jackets/2.jpeg"),
-  require("./images/jackets/3.jpeg"),
-  require("./images/jackets/4.jpeg"),
-  require("./images/jackets/5.jpeg"),
-  require("./images/jackets/6.jpeg"),
-  require("./images/jackets/7.jpeg")
-
-]
+let pantsPath = pantsArr.map(x=>pantsFiles(x));
+let shirtsPath = shirtsArr.map(x=>shirtsFiles(x));
+let jacketsPath = jacketsArr.map(x=>jacketsFiles(x));
 
 //--The function below takes the array length for each dir of pics and pics a random one.
-//--That value is stored in a new array and is passed to the ImageDisplay component.
+//--That value is stored in a new array ais passeod to the ImageDisplay component.
 const todayGear = () =>{
-  let thePics = [];
-  let x = Math.floor(Math.random() * pants.length); 
-  let y = Math.floor(Math.random() * shirts.length); 
-  let z = Math.floor(Math.random() * jackets.length);
 
-  thePics.push(pants[x],shirts[y],jackets[z]);
-  return thePics  
+
+  let thePics = [];
+  let x = Math.floor(Math.random() * pantsArr.length); 
+  let y = Math.floor(Math.random() * shirtsArr.length); 
+  let z = Math.floor(Math.random() * jacketsArr.length);
+
+  thePics.push(pantsFiles(pantsArr[x]),shirtsFiles(shirtsArr[y]),jacketsFiles(jacketsArr[z]));
+
+  return thePics;  
   
 }
 
@@ -68,7 +47,8 @@ class App extends React.Component{
   render() {
     return (
     <div className={sty.App}>
-      <h1>Today's Gear </h1>
+      <Dropdown />
+      <h1>Leonardo's Best Code</h1>
       <div>
         <button className={sty.button} onClick={this.hc.bind(this)} > New Gear </button>
       </div>
@@ -77,13 +57,13 @@ class App extends React.Component{
       </div>
       
       <div className={sty.mainImageDis}>
-        <ImageGallery1 imageUrls={pants} />
+        <ImageGallery1 imageUrls={pantsPath} />
       </div>
       <div className={sty.mainImageDis}>
-        <ImageGallery2 imageUrls={shirts} />
+        <ImageGallery2 imageUrls={shirtsPath} />
       </div>
       <div className={sty.mainImageDis}>
-        <ImageGallery3 imageUrls={jackets} />
+        <ImageGallery3 imageUrls={jacketsPath} />
       </div>
 
     </div>
